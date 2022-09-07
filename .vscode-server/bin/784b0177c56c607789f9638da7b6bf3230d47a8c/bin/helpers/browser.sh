d90b9d8ab62865e316e0e6927e295133c309e339
CMENDOZA1036 <alvarezmendoz@gmail.com>
2022-09-07 18:59:42 -0500

    Updated 2

---
+++ .vscode-server/bin/784b0177c56c607789f9638da7b6bf3230d47a8c/bin/helpers/browser.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+#
+ROOT="$(dirname "$(dirname "$(dirname "$(readlink -f "$0")")")")"
+
+APP_NAME="code"
+VERSION="1.71.0"
+COMMIT="784b0177c56c607789f9638da7b6bf3230d47a8c"
+EXEC_NAME="code"
+CLI_SCRIPT="$ROOT/out/server-cli.js"
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "--openExternal" "$@"
